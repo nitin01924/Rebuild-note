5feeedf3024f995f2b873aaf305f62e6734802c6
nitin kumar <nitin981275@gmail.com>
2026-05-11 21:14:47 +0530

    fix: fixing the User Schema

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -6,11 +6,13 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
     },
     password: {
       type: String,
