db8eb1e7a48bd7e306aa8cce41f6acf1b4db6f65
nitin kumar <nitin981275@gmail.com>
2026-06-03 13:51:08 +0530

    refactor: using try/catch for simplecity

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -5,34 +5,41 @@ import crypto from "crypto";
 const app = express();
 
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    return res.status(400).json({
-      message: "Please fill all the fields",
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Please fill all the fields",
+      });
+    }
+    const normalizedEmail = email.toLowerCase();
+
+    const existingEmail = await User.findOne({ email: normalizedEmail });
+    if (existingEmail) {
+      return res.status(400).json({
+        success: false,
+        error: "User already exist.",
+      });
+    }
+
+    const token = crypto.randomBytes(32).toString("hex");
+    const user = await User.create({
+      name,
+      email: normalizedEmail,
+      password,
+      verificationToken: token,
     });
-  }
-  const normalizedEmail = email.toLowerCase();
-
-  const existingEmail = await User.findOne({ email: normalizedEmail });
-  if (existingEmail) {
-    return res.status(400).json({
+    res.status(201).json({
+      success: true,
+      message: "User has been Registered",
+    });
+  } catch (error) {
+    res.status(500).json({
       success: false,
-      error: "User already exist.",
+      message: "something goes wrong with backend",
     });
   }
-
-  const token = crypto.randomBytes(32).toString("hex");
-  const user = await User.create({
-    name,
-    email: normalizedEmail,
-    password,
-    verificationToken: token,
-  });
-  res.status(201).json({
-    success: true,
-    message: "User has been Registered",
-  });
 };
 
 export default registerUser;
